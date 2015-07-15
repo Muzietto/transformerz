077e7cdefa4ee228259b7bd720a83c8a8168b331
Muzietto <marco_faustinelli@yahoo.com>
2015-07-15 06:34:14 +0200

    WriterT working like a charm

--- haskell/nilsson/Nilsson_02.hs
+++ haskell/nilsson/Nilsson_02.hs
@@ -36,3 +36,68 @@ module Nilsson_02 where
   instance (Monoid w, Functor m, Monad m) => Functor (WT w m) where
     fmap fab wtwma = WT $ do (a, w) <- unWT wtwma
                              return (fab a, w)
+
+---------------------------------------------
+  -- kind of MonadWriter
+  class (Monoid w, Monad m) => W w m | m -> w where
+    -- tell w is an action that produces the output w
+    tell :: w -> m ()
+    -- listen m is an action that executes action m and adds its output to the value of the computation
+    listen :: m a -> m (a, w)
+    -- pass m is an action that executes action m (which returns a value and a function) and returns the value applying the function to the output
+    -- pass :: m (a, w -> w) -> m a 
+
+  instance (Monoid w, Monad m) => W w (WT w m) where
+    --tell :: (Monoid w, Monad m) => w -> WT w m ()
+    tell msg = WT $ return ((), msg)
+    --listen :: (Monoid w, Monad m) => WT w m a -> WT w m (a, w)
+    listen ma = WT $ do (a, w) <- unWT ma
+                        return ((a, w), w)
+--    listen ma = do a <- ma
+  --                 return (a, mempty)
+
+  instance (Monoid w, Monad m) => MonadTransformer (WT w) m where
+    -- lift :: m a -> t m a
+    -- lift :: m a -> WT w m a = WT (m (a, w))
+    lift ma = WT $ do a <- ma
+                      return (a, mempty)
+
+------------- ET + WT -----------------
+
+  type Eval4c a = ET (WT [String] I) a
+             -- = ET (WT [String] I) (Maybe a)
+             -- = ET (WT I(Maybe a, [String]))
+          
+  runEval4c :: Eval4c Value -> (Maybe Value, [String])
+  runEval4c etwticmaybevalstrarra = 
+            unI $ unWT $ unET etwticmaybevalstrarra
+            
+  eval4c :: Env -> Exp -> Eval4c Value
+  eval4c env (Lit i) = do lift $ tell ["literal"]
+                          return $ IntVal i
+  eval4c env (Var name) = do lift $ tell ["lookup " ++ name]
+                             case (Map.lookup name env) of
+                               Just val -> do lift $ tell ["ok"]
+                                              return $ val
+                               Nothing -> do lift $ tell ["ko"]
+                                             eFail
+  eval4c env (Plus e1 e2) = 
+       do lift $ tell ["sum"]
+          v1 <- eval4c env e1
+          v2 <- eval4c env e2
+          case (v1, v2) of
+            (IntVal i1, IntVal i2) -> do lift $ tell ["ok"]
+                                         return $ IntVal (i1 + i2)
+            _ -> do lift $ tell ["ko"]
+                    eFail
+  eval4c env (Lambda argname body) =
+       do lift $ tell ["lambda"]
+          return $ FunVal argname body env
+
+
+
+
+
+--
+
+
--- haskell/nilsson/TestNilsson_02.hs
+++ haskell/nilsson/TestNilsson_02.hs
@@ -23,14 +23,73 @@ module TestNilsson_02 where
 ----------------------------
 
 
+  testEval4cLiteral :: Test
+  testEval4cLiteral = 
+      TestCase $ assertEqual "eval4c should evaluate a silly literal"
+                             (Just (IntVal 18), ["literal"]) (runEval4c $ eval4c Map.empty (Lit 18))
+{-
   testEval3SimpleApp :: Test
   testEval3SimpleApp = 
       TestCase $ assertEqual "eval3 should make a simple application"
                              (Just (IntVal 18), 8) (runEval3 0 $ eval3 Map.empty sample)
 
+  testEval3ComplexApp :: Test
+  testEval3ComplexApp = 
+      TestCase $ assertEqual "eval3 should make a complex application"
+                             (Just (IntVal 127), 9) (runEval3 0 $ eval3 two_vars_env samplone)
 
+  testEval3CurriedApp :: Test
+  testEval3CurriedApp = 
+      TestCase $ assertEqual "eval3 should make a partial application"
+                             (Just (FunVal "y" (Plus (Var "x") (Var "y")) (Map.fromList [("x",IntVal 4)])), 4) 
+                             (runEval3 0 $ eval3 Map.empty (App lambdona (Lit 4)))
+
+  testEval3CurriedApp2 :: Test
+  testEval3CurriedApp2 = 
+      TestCase $ assertEqual "eval3 should spit Nothing in case of errore"
+                             (Nothing, 3) 
+                             (runEval3 0 $ eval3 Map.empty (App lambdona (Var "inesistente")))
+-}
+  testEval4cWatIsXPlusY :: Test
+  testEval4cWatIsXPlusY = 
+      TestCase $ assertEqual "eval4c should sum two vars"
+                             (Just (IntVal 357), ["sum",
+                                                  "lookup xxxx",
+                                                  "ok",
+                                                  "lookup yyyy",
+                                                  "ok",
+                                                  "ok"
+                                                  ]) (runEval4c $ eval4c two_vars_env xPlusY)
+
+  testEval4cWatIsXPlusCrash :: Test
+  testEval4cWatIsXPlusCrash = 
+      TestCase $ assertEqual "eval4c should fail summing stuff when one ain't IntVal"
+                             (Nothing, ["sum",
+                                       "literal",
+                                       "lambda",
+                                       "ko"
+                             ]) (runEval4c $ eval4c Map.empty (Plus (Lit 123) lambdina))
+        
+  testEval4cVarUndefined :: Test
+  testEval4cVarUndefined = 
+      TestCase $ assertEqual "eval4c should fail on non-existing vars and write about it"
+        (Nothing, ["lookup zzzz","ko"])
+        (runEval4c (eval4c two_vars_env (Var "zzzz")))
+
+  testEval4cVarXxxx :: Test
+  testEval4cVarXxxx = 
+      TestCase $ assertEqual "eval4c should lookup a Var and write about it"
+        (Just (IntVal 123), ["lookup xxxx","ok"]) 
+        (runEval4c (eval4c two_vars_env watIsXxxx))
+        
+
+        
   main :: IO Counts
   main = runTestTT $ TestList [
 
-    testEval3SimpleApp
+    testEval4cLiteral,
+    testEval4cVarXxxx,
+    testEval4cVarUndefined,
+    testEval4cWatIsXPlusY,
+    testEval4cWatIsXPlusCrash
                                ]
